be3913273d7da9393f5bd82c95b7d29caaa756a7
seekaddo <dennis_addo@aol.com>
2020-12-18 09:26:21 +0100

    backend test cleanup done

--- src/tests/BackendTest.java
+++ src/tests/BackendTest.java
@@ -1,13 +1,16 @@
 package tests;
 import static org.junit.jupiter.api.Assertions.*;
 
+import fh.campus.asd.battleship.backend.models.Field;
 import fh.campus.asd.battleship.backend.models.Player;
 import fh.campus.asd.battleship.backend.models.Ship;
 import fh.campus.asd.battleship.backend.models.ShipPart;
+import fh.campus.asd.battleship.forontend.enums.Direction;
 import org.junit.jupiter.api.Test;
 
 public class BackendTest {
 
+    Player player1 = new Player();
 
     @Test
     void TestShipPart() {
@@ -29,7 +32,7 @@ public class BackendTest {
     @Test
     void TestPlayer() {
 
-        Player player1 = new Player();
+
         player1.saveAttack(2,3);
         player1.saveAttack(3,5);
         player1.saveAttack(7,2);
@@ -45,9 +48,38 @@ public class BackendTest {
     }
 
     @Test
-    void TestShipr() {
+    void TestShip() {
 
-        //Ship ship1
+        Direction direction1 = Direction.RIGHT;
+        Ship ship1 = new Ship(3,4,10,direction1,4,6);
+
+        assertEquals(Direction.RIGHT, ship1.getDirection() );
+
+        assertNotEquals(4,ship1.getDivy());
+
+        assertEquals(10, ship1.getLength());
+
+        assertFalse(ship1.checkIfDestroyed());
+
+        assertFalse(ship1.attack(5,6));
+
+        assertFalse(ship1.checkIfDestroyed());
+
+        assertTrue(ship1.attack(3,4));
+
+        assertFalse(ship1.checkIfDestroyed());
+
+
+    }
+
+    @Test
+    void TestField() {
+
+        Field field1 = player1.getArea();
+
+        assertFalse(field1.isFleetComplete());
+        assertTrue(field1.gameOver());
+
 
     }
 
